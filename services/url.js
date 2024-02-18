9ca69df4d0909a3e7ee6d34883bb003ac5b7a402
Fakhrul Azhim <achim.keren@gmail.com>
2024-02-18 07:11:49 +0700

    new path

--- services/url.js
+++ services/url.js
@@ -1,4 +1,4 @@
 export const baseUrl = 'https://editorial.rent/wp-json/wp/v2';
 export const theme = 1;
-export const postsPath = 'fun';
-export const categoriesPath = 'funcategory';
+export const postsPath = 'monster';
+export const categoriesPath = 'monster-category';
